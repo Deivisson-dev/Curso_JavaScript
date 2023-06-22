ee363e09f8b8aa376a154027f55cf3b874422bf0
Deivisson-dev <darsonsilva83@gmail.com>
2023-06-22 17:05:00 -0300

    arrays and objects

---
+++ revisão-lógica/arrays.js
@@ -0,0 +1,8 @@
+const arr = new Array()
+const arr2 = new Array(true, "Bom dia", 10, 9, new Array(5,45))
+arr[0] = "Deivisson"
+arr[1] = 36
+arr.push(25)
+console.log(arr)
+console.log(arr[arr.length - 1])
+console.log(arr2[4][0])
