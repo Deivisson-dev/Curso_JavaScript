93fe02ab4d29cf671e2475f4607b66ff8445599c
Deivisson-dev <darsonsvila83@gmail.com>
2023-07-14 10:48:37 -0300

    learning about functions

---
+++ funcoes/iife-menu.js
@@ -0,0 +1,11 @@
+
+
+(function (n1, n2, n3) {
+    let isValid = false;
+    console.log("menu", isValid, n1, n2, n3)
+
+    function init() {
+        console.log('init do menu ');
+    }
+    init()
+})(78, 25, 45)
---
+++ funcoes/iife-modal.js
@@ -0,0 +1,10 @@
+
+(function () {
+    let isValid = true;
+    console.log("modal", isValid)
+
+    function init() {
+        console.log('init do modal');
+    }
+    init()
+})()
