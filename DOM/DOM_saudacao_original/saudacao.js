176f6318a1ce3f137a99fa0916aecc4524ea5414
Deivisson-dev <darsonsilva83@gmail.com>
2023-10-23 00:42:01 -0300

    add DOM exercises

--- DOM/DOM_saudacao_original/saudacao.js
+++ DOM/DOM_saudacao_original/saudacao.js
@@ -1,9 +1,21 @@
 (function (){
-    const nameUser = "Deivisson";
+    const nameUser = "Manuel"
     const elemento = document.querySelector('.top-bar p');
     if (nameUser){
         elemento.textContent += `${nameUser}`;
     } else {
-        elemento.parentElement.style.display = 'none';
+        elemento.parentElement.remove();
     }
 })();
+
+
+function submeter(){
+    const email_digitado = document.querySelector('#email').value;
+    const msg_na_tela = document.getElementById('mensagem');
+    if (email_digitado){
+        msg_na_tela.innerHTML = `O email ${email_digitado} foi cadastrado com sucesso!`;
+    }
+    else {
+        msg_na_tela.innerHTML = `Por favor, preencha o campo de email!`;
+    }
+}
